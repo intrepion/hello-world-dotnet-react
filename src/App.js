a3d0cf2b53c2f6082f04f556ad5db63f9b234c13
Oliver Forral <intrepion@gmail.com>
2023-11-03 01:31:39 -0600

    green - Adding posts.

--- src/App.js
+++ src/App.js
@@ -14,8 +14,23 @@ constructor(props) {
     .then(json => this.setState({ posts: json }))
   }
   render() {
+    const { posts } = this.state;
     return (
-      <p>Hello world!</p>
+      <div className="container">
+        <div class="jumbotron">
+          <h1 class="display-4">Posts from our API call</h1>
+        </div>
+        {posts.map((post) => (
+          <div className="card" key={post.id}>
+            <div className="card-header">
+              ID #{post.id} {post.title}
+            </div>
+            <div className="card-body">
+              <img src={post.thumbnailUrl}></img>
+            </div>
+          </div>
+        ))}
+      </div>
     );
   }
 }
