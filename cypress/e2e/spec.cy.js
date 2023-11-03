b3318908d3ea8c9a4a643f687e6132f401226a72
Oliver Forral <intrepion@gmail.com>
2023-11-02 20:06:14 -0600

    red - Replaced app code.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
     cy.visit('/')
+
+    cy.get("p")
+      .should("contain", "Hello world! This is my first React app.");
   })
 })
