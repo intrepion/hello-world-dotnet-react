c4444f3ed33c6a547ba1dc5894c913c3cc5ba3ab
Oliver Forral <intrepion@gmail.com>
2023-11-02 20:45:09 -0600

    blue - Made an API call.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ constructor(props) {
       posts: []
     }
   }
+  componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/albums/1/photos";
+    fetch(url)
+    .then(response => response.json())
+    .then(json => this.setState({ posts: json }))
+  }
   render() {
     return (
       <p>Hello world!</p>
