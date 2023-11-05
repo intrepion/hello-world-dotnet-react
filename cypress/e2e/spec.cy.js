45e66f59ca1e0286ecb4e6f73d63493bfbefc852
Oliver Forral <intrepion@gmail.com>
2023-11-05 09:00:40 -0700

    Testing home page.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('The Home Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/')
   })
 })
