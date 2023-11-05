0e661f944323ea8d92c53eec3358a0a18e057d3a
Oliver Forral <intrepion@gmail.com>
2023-11-05 11:09:41 -0700

    Switching localhost to 127.0.0.1 in some places.

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://localhost:44459',
+    baseUrl: 'https://127.0.0.1:44459',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
